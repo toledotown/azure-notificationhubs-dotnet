059fa7074db3035b51f3bb5872d323cf1cca8dc1
Steve Flowers <toledotown@gmail.com>
2019-08-13 18:40:29 -0400

    sf edits

--- Samples/AzFunctions/AzFunctionsSample/Parameters.cs
+++ Samples/AzFunctions/AzFunctionsSample/Parameters.cs
@@ -2,6 +2,6 @@
 {
     public static class Parameters
     {
-        public static string ConnectionString = "<your Notification Hubs ConnectionString>";
+        public static string ConnectionString = "Endpoint=sb://veraapp.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=F7UxW0MHciEEW6/wJbQ46pd02yX1JpeDtIf/sxI4L4A=";
     }
 }
--- Samples/RegistrationSample/RegistrationSample/Program.cs
+++ Samples/RegistrationSample/RegistrationSample/Program.cs
@@ -17,52 +17,107 @@ namespace RegistrationSample
             // Getting connection key from the new resource
             var config = LoadConfiguration(args);
             var nhClient = NotificationHubClient.CreateClientFromConnectionString(config.PrimaryConnectionString, config.HubName);
-            await CreateAndDeleteInstallationAsync(nhClient);
-            await CreateAndDeleteRegistrationAsync(nhClient);
+            await CreateInstallationAsync(nhClient);
+            //await CreateAndDeleteInstallationAsync(nhClient);
+            //await CreateAndDeleteRegistrationAsync(nhClient);
         }
 
         private static async Task CreateAndDeleteRegistrationAsync(NotificationHubClient nhClient)
         {
             var registrationId = await nhClient.CreateRegistrationIdAsync();
-            var registrationDescr = await nhClient.CreateFcmNativeRegistrationAsync(registrationId);
-            Console.WriteLine($"Created FCM registration {registrationDescr.FcmRegistrationId}");
+            //var registrationDescr = await nhClient.CreateFcmNativeRegistrationAsync(registrationId);
+            var registrationDescr = await nhClient.CreateAppleNativeRegistrationAsync(registrationId);
+            //Console.WriteLine($"Created APNS registration {registrationDescr.FcmRegistrationId}");
+            Console.WriteLine($"Create APNS registration {registrationDescr.RegistrationId}");
 
             var allRegistrations = await nhClient.GetAllRegistrationsAsync(1000);
             foreach (var regFromServer in allRegistrations)
             {
                 if (regFromServer.RegistrationId == registrationDescr.RegistrationId)
                 {
-                    Console.WriteLine($"Found FCM registration {registrationDescr.FcmRegistrationId}");
+                    //Console.WriteLine($"Found FCM registration {registrationDescr.FcmRegistrationId}");
+                    Console.WriteLine($"Found APNS registration {registrationDescr.RegistrationId}");
                     break;
                 }
             }
 
+            //commented out by code creator (next 2 lines)
             //registrationDescr = await nhClient.GetRegistrationAsync<FcmRegistrationDescription>(registrationId);
             //Console.WriteLine($"Retrieved FCM registration {registrationDescr.FcmRegistrationId}");
 
             await nhClient.DeleteRegistrationAsync(registrationDescr);
-            Console.WriteLine($"Deleted FCM registration {registrationDescr.FcmRegistrationId}");
+            //Console.WriteLine($"Deleted FCM registration {registrationDescr.FcmRegistrationId}");
+            Console.WriteLine($"Deleted APNS registration {registrationDescr.RegistrationId}");
+        }
+
+        private static async Task CreateInstallationAsync(NotificationHubClient nhClient)
+        {
+            // Register some fake devices
+            //var fcmDeviceId = Guid.NewGuid().ToString();
+            //var apnsDeviceId = "djPUqVGjd9I:APA91bE7UyqaMr0IAICU7Wz5eV5qUEaZ2nEA7YkFcz6UqCHv-nIkUNDBAE4cZdjjG5bw8WnT2Psu7-eV4GMURVXtMGzI_yju2VRAzaTShW-9xwW2qy9D_SPIRsD8xVLalVklQgy_xara";
+            var apnsDeviceId = "c61cd9eea3d2def2";
+            Console.WriteLine($"The APNS device ID is {apnsDeviceId}");
+                //var fcmInstallation = new Installation
+                var apnsInstallation = new Installation
+                {
+                    //InstallationId = fcmDeviceId,
+                    InstallationId = apnsDeviceId,
+                    //Platform = NotificationPlatform.Fcm,
+                    Platform = NotificationPlatform.Apns,
+                    //PushChannel = fcmDeviceId,
+                    PushChannel = apnsDeviceId,
+                    PushChannelExpired = false,
+                    //Tags = new[] { "fcm" }
+                    Tags = new[] { "apns" }
+                };
+                //await nhClient.CreateOrUpdateInstallationAsync(fcmInstallation);
+                await nhClient.CreateOrUpdateInstallationAsync(apnsInstallation);
+
+                while (true)
+                {
+                    try
+                    {
+                        //var installationFromServer = await nhClient.GetInstallationAsync(fcmInstallation.InstallationId);
+                        var installationFromServer = await nhClient.GetInstallationAsync(apnsInstallation.InstallationId);
+                        break;
+                    }
+                    catch (MessagingEntityNotFoundException)
+                    {
+                        // Wait for installation to be created
+                        await Task.Delay(1000);
+                    }
+                }
+                //Console.WriteLine($"Created FCM installation {fcmInstallation.InstallationId}");
+                Console.WriteLine($"Created APNS installation {apnsInstallation.InstallationId}");
         }
 
         private static async Task CreateAndDeleteInstallationAsync(NotificationHubClient nhClient)
         {
             // Register some fake devices
-            var fcmDeviceId = Guid.NewGuid().ToString();
-            var fcmInstallation = new Installation
+            //var fcmDeviceId = Guid.NewGuid().ToString();
+            var apnsDeviceId = Guid.NewGuid().ToString();
+            Console.WriteLine($"The APNS device ID is {apnsDeviceId}");
+            //var fcmInstallation = new Installation
+            var apnsInstallation = new Installation
             {
-                InstallationId = fcmDeviceId,
+                //InstallationId = fcmDeviceId,
+                InstallationId = apnsDeviceId,
                 Platform = NotificationPlatform.Fcm,
-                PushChannel = fcmDeviceId,
+                //PushChannel = fcmDeviceId,
+                PushChannel = apnsDeviceId,
                 PushChannelExpired = false,
-                Tags = new[] { "fcm" }
+                //Tags = new[] { "fcm" }
+                Tags = new[] { "apns" }
             };
-            await nhClient.CreateOrUpdateInstallationAsync(fcmInstallation);
+            //await nhClient.CreateOrUpdateInstallationAsync(fcmInstallation);
+            await nhClient.CreateOrUpdateInstallationAsync(apnsInstallation);
 
             while (true)
             {
                 try
                 {
-                    var installationFromServer = await nhClient.GetInstallationAsync(fcmInstallation.InstallationId);
+                    //var installationFromServer = await nhClient.GetInstallationAsync(fcmInstallation.InstallationId);
+                    var installationFromServer = await nhClient.GetInstallationAsync(apnsInstallation.InstallationId);
                     break;
                 }
                 catch (MessagingEntityNotFoundException)
@@ -71,18 +126,22 @@ namespace RegistrationSample
                     await Task.Delay(1000);
                 }
             }
-            Console.WriteLine($"Created FCM installation {fcmInstallation.InstallationId}");
-            await nhClient.DeleteInstallationAsync(fcmInstallation.InstallationId);
+            //Console.WriteLine($"Created FCM installation {fcmInstallation.InstallationId}");
+            Console.WriteLine($"Created APNS installation {apnsInstallation.InstallationId}");
+            //await nhClient.DeleteInstallationAsync(fcmInstallation.InstallationId);
+            await nhClient.DeleteInstallationAsync(apnsInstallation.InstallationId);
             while (true)
             {
                 try
                 {
-                    var installationFromServer = await nhClient.GetInstallationAsync(fcmInstallation.InstallationId);
+                    //var installationFromServer = await nhClient.GetInstallationAsync(fcmInstallation.InstallationId);
+                    var installationFromServer = await nhClient.GetInstallationAsync(apnsInstallation.InstallationId);
                     await Task.Delay(1000);
                 }
                 catch (MessagingEntityNotFoundException)
                 {
-                    Console.WriteLine($"Deleted FCM installation {fcmInstallation.InstallationId}");
+                    //Console.WriteLine($"Deleted FCM installation {fcmInstallation.InstallationId}");
+                    Console.WriteLine($"Deleted APNS installation {apnsInstallation.InstallationId}");
                     break;
                 }
             }
