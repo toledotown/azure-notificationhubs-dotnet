ebfc3896a1f05f57e905fe9742414b74313c6355
Steve Flowers <steve.flowers@o-i.com>
2019-08-13 22:15:04 -0400

    sf edits home

--- Samples/RegistrationSample/RegistrationSample/Program.cs
+++ Samples/RegistrationSample/RegistrationSample/Program.cs
@@ -18,6 +18,7 @@ namespace RegistrationSample
             var config = LoadConfiguration(args);
             var nhClient = NotificationHubClient.CreateClientFromConnectionString(config.PrimaryConnectionString, config.HubName);
             await CreateInstallationAsync(nhClient);
+            //await CreateRegistrationAsync(nhClient);
             //await CreateAndDeleteInstallationAsync(nhClient);
             //await CreateAndDeleteRegistrationAsync(nhClient);
         }
@@ -50,18 +51,54 @@ namespace RegistrationSample
             Console.WriteLine($"Deleted APNS registration {registrationDescr.RegistrationId}");
         }
 
+        private static async Task CreateRegistrationAsync(NotificationHubClient nhClient)
+        {
+            var registrationId = await nhClient.CreateRegistrationIdAsync();
+            //var registrationDescr = await nhClient.CreateFcmNativeRegistrationAsync(registrationId);
+            var registrationDescr = await nhClient.CreateAppleNativeRegistrationAsync(registrationId);
+            //Console.WriteLine($"Created APNS registration {registrationDescr.FcmRegistrationId}");
+            //registrationDescr.RegistrationId = "96d6bb5d412881d30046f5bf5616beaddf6aa4abe57987bccc4b5c23209dbb0c";
+            registrationDescr.RegistrationId = "djPUqVGjd9I:APA91bE7UyqaMr0IAICU7Wz5eV5qUEaZ2nEA7YkFcz6UqCHv-nIkUNDBAE4cZdjjG5bw8WnT2Psu7-eV4GMURVXtMGzI_yju2VRAzaTShW-9xwW2qy9D_SPIRsD8xVLalVklQgy_xara";
+            Console.WriteLine($"Create APNS registration {registrationDescr.RegistrationId}");
+
+            var allRegistrations = await nhClient.GetAllRegistrationsAsync(1000);
+            foreach (var regFromServer in allRegistrations)
+            {
+                if (regFromServer.RegistrationId == registrationDescr.RegistrationId)
+                {
+                    //Console.WriteLine($"Found FCM registration {registrationDescr.FcmRegistrationId}");
+                    Console.WriteLine($"Found APNS registration {registrationDescr.RegistrationId}");
+                    break;
+                }
+            }
+
+            //commented out by code creator (next 2 lines)
+            //registrationDescr = await nhClient.GetRegistrationAsync<FcmRegistrationDescription>(registrationId);
+            //Console.WriteLine($"Retrieved FCM registration {registrationDescr.FcmRegistrationId}");
+
+        }
+
         private static async Task CreateInstallationAsync(NotificationHubClient nhClient)
         {
             // Register some fake devices
             //var fcmDeviceId = Guid.NewGuid().ToString();
+            var InstallationId = Guid.NewGuid().ToString();
             //var apnsDeviceId = "djPUqVGjd9I:APA91bE7UyqaMr0IAICU7Wz5eV5qUEaZ2nEA7YkFcz6UqCHv-nIkUNDBAE4cZdjjG5bw8WnT2Psu7-eV4GMURVXtMGzI_yju2VRAzaTShW-9xwW2qy9D_SPIRsD8xVLalVklQgy_xara";
-            var apnsDeviceId = "c61cd9eea3d2def2";
+            //var apnsDeviceId = "96d6bb5d-412881d3-0046f5bf-5616bead-df6aa4ab-e57987bc-cc4b5c23-209dbb0c";
+            //var apnsDeviceId = "96d6bb5d412881d30046f5bf5616beaddf6aa4abe57987bccc4b5c23209dbb0c";
+            var apnsDeviceId = "c6a0cef6e29d7b38040c843d2296b7ecdf3d66f5d1b62e3cffb64a2edadd6d05";
+            //var apnsDeviceId = "37303132443544373636453344373635444530313746363541353845304446423436314136423737394335374236433636303034304636343935454132304142";
+            //c6a0cef6 e29d7b38 040c843d 2296b7ec df3d66f5 d1b62e3c ffb64a2e dadd6d05
+            //c6a0cef6e29d7b38040c843d2296b7ecdf3d66f5d1b62e3cffb64a2edadd6d05
+            //var apnsDeviceId = "c61cd9eea3d2def2";
+            Console.WriteLine($"The Installation ID is {InstallationId}");
             Console.WriteLine($"The APNS device ID is {apnsDeviceId}");
                 //var fcmInstallation = new Installation
                 var apnsInstallation = new Installation
                 {
                     //InstallationId = fcmDeviceId,
-                    InstallationId = apnsDeviceId,
+                    //InstallationId = apnsDeviceId,
+                    InstallationId = InstallationId,
                     //Platform = NotificationPlatform.Fcm,
                     Platform = NotificationPlatform.Apns,
                     //PushChannel = fcmDeviceId,
